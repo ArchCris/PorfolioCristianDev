c5d4abc513b74df07b1fd870988826a291d59c0a
unknown <you@example.comarq.cristianmasci@gmail.comgit config --global user.name Your>
2021-07-11 17:45:42 -0300

    Avances contact

--- app.js
+++ app.js
@@ -43,5 +43,98 @@ window.onload = () => {
             offsetTop: 20
         });
     });
-});
+ });
 
+//RESET FORM//
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+}
+  
+//FORM VALIDATION//
+const nameOk = document.querySelector("#nameOk")
+const mailOk = document.querySelector("#mailOk")
+const nameWrong = document.querySelector("#nameWrong")
+const mailWrong = document.querySelector("#mailWrong")
+
+const inputName = document.querySelector("#name")
+const inputMail = document.querySelector("#mail")
+const textarea = document.querySelector("#textarea")
+const btnSubmit = document.querySelector("#btnSubmit")
+
+const nameErrorConteiner = document.querySelector(".nameErrorConteiner")
+const mailErrorConteiner = document.querySelector(".mailErrorConteiner")
+
+
+let submitStatus = true
+
+inputName.addEventListener("blur", () => {
+
+    let errorMessage=[]
+    
+    if(inputName.value.length <= 6 && inputName.value != "" ){
+        errorMessage.push("The name should have more than 6 characters")
+        nameOk.classList.remove("visible")
+        nameWrong.classList.add("visible")
+        submitStatus = false
+    } else {
+        nameWrong.classList.remove("visible")
+        nameOk.classList.add("visible")
+        submitStatus = true
+    }
+    if(/[0-9]/.test(inputName.value) == true){
+        errorMessage.push(" The name should not have numbers")
+        nameOk.classList.remove("visible")
+        nameWrong.classList.add("visible")
+        submitStatus = false
+    }
+    if(inputName.value == ""){
+        errorMessage.push(" Write something in the input")
+        nameOk.classList.remove("visible")
+        nameWrong.classList.add("visible")
+        submitStatus = false
+    }
+    nameErrorConteiner.innerHTML = errorMessage 
+})
+
+inputMail.addEventListener("blur", () => {
+
+    let errorMessage = []
+
+    if(/@/.test(inputMail.value) == false && inputMail.value != ""){
+        errorMessage.push(" The email address must contain @")
+        mailOk.classList.remove("visible")
+        mailWrong.classList.add("visible")
+        submitStatus = false
+    } else {
+        mailWrong.classList.remove("visible")
+        mailOk.classList.add("visible")
+        submitStatus = true
+        
+    }
+
+    if( inputMail.value == ""){
+        errorMessage.push(" Write something in the input")
+        mailOk.classList.remove("visible")
+        mailWrong.classList.add("visible")
+        submitStatus = false
+    } 
+    
+    console.log(submitStatus)
+    mailErrorConteiner.innerHTML = errorMessage 
+})
+
+btnSubmit.addEventListener("click", (e) => {
+    if (textarea.value == "") {
+        textarea.style.border = "solid 4px rgb(197, 78, 78)"
+    }if (inputName.value == "") {
+        console.log("g")
+        nameWrong.classList.add("visible")
+    }if(inputMail.value == "") {
+        mailWrong.classList.add("visible")
+    }
+    if (submitStatus == false) {
+        e.preventDefault()
+    }
+})
